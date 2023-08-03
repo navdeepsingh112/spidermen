af521293036193e1cdb00bf689a83825d46b122c
Vaibhav <baldevaibhav@gmail.com>
2023-08-04 00:51:24 +0530

    Spideman spacebar

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-console.log("Hi")
-
+console.log("Hi");
+function jump()
+{
+ var top=window.getComputedStyle(document.getElementById(`spidermans`),null).getPropertyValue(`top`);
+ var left=window.getComputedStyle(document.getElementById(`spidermans`),null).getPropertyValue(`left`);
+var sp=document.getElementById(`spidermans`);
+sp.style.top='calc('+top+' - 1px)';
+sp.style.left='calc('+left+' - 1px)';
+}
 document.getElementById("spiderman").addEventListener('keyup', function(e)
 {
    if(e.keyCode == 32)
    {
+      var sp=document.getElementById(`spidermans`);
+         console.log(sp.style.top);
     console.log("jump");
     var abc=window.getComputedStyle(document.getElementById(`spidermans`),null).getPropertyValue(`transform`);
     var value=abc.split(`(`)[1];
@@ -12,7 +21,8 @@ document.getElementById("spiderman").addEventListener('keyup', function(e)
     var r=Math.atan2(value[1], value[0]);
 
     var angle=Math.round(r*(180/Math.PI));
-    
+   // jump();
+     setInterval(jump,40);
 
    }
    else{
